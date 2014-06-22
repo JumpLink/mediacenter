b7648c52aefe04f131f968b110c53ecd788a5875
JumpLink <jumplink@gmail.com>
2014-06-22 11:23:28 +0200

    Added support to play videos with ffplay for tests and systems without omxplayer

--- src/api/controllers/FFPlayController.js
+++ src/api/controllers/FFPlayController.js
@@ -7,19 +7,19 @@
 
 
 
-var omx = require('omxcontrol');
+var ffplay = require('ffplaycontrol');
 var Path = require('path')
 
 module.exports = {
   /**
-   * `OMXController.start()`
+   * `ffplayController.start()`
    */
   start: function (req, res) {
     var path = req.param('id') ? req.param('id') : req.param('filename') ? req.param('filename') : req.param('path') ? req.param('path') : null;
     if(path != null) {
       path = Path.normalize(path);
       sails.log.debug("start "+path);
-      omx.start(path);
+      ffplay.start(path);
       return res.ok();
     } else {
       return res.serverError("No path");
@@ -27,95 +27,59 @@ module.exports = {
   }
 
   /**
-   * `OMXController.pause()`
+   * `ffplayController.pause()`
    */
   , pause: function (req, res) {
     sails.log.debug("pause");
-    omx.pause();
+    ffplay.pause();
     return res.ok();
   }
 
   , play: function (req, res) {
     sails.log.debug("play");
-    omx.play();
-    return res.ok();
-  }
-
-  , volume_up: function (req, res) {
-    sails.log.debug("volume_up");
-    omx.volume_up();
-    return res.ok();
-  }
-
-  , volume_down: function (req, res) {
-    sails.log.debug("volume_down");
-    omx.volume_down();
+    ffplay.play();
     return res.ok();
   }
 
   , quit: function (req, res) {
     sails.log.debug("quit");
-    omx.quit();
+    ffplay.quit();
     return res.ok();
   }
 
   , forward: function (req, res) {
     sails.log.debug("forward");
-    omx.forward();
+    ffplay.forward();
     return res.ok();
   }
 
   , backward: function (req, res) {
     sails.log.debug("backward");
-    omx.backward();
+    ffplay.backward();
     return res.ok();
   }
 
   , next_subtitle: function (req, res) {
     sails.log.debug("next_subtitle");
-    omx.next_subtitle();
-    return res.ok();
-  }
-
-  , previous_subtitle: function (req, res) {
-    sails.log.debug("previous_subtitle");
-    omx.previous_subtitle();
-    return res.ok();
-  }
-
-  , next_chapter: function (req, res) {
-    sails.log.debug("next_chapter");
-    omx.next_chapter();
-    return res.ok();
-  }
-
-  , previous_chapter: function (req, res) {
-    sails.log.debug("previous_chapter");
-    omx.previous_chapter();
+    ffplay.next_subtitle();
     return res.ok();
   }
 
   , next_audio: function (req, res) {
     sails.log.debug("next_audio");
-    omx.next_audio();
+    ffplay.next_audio();
     return res.ok();
   }
 
-  , previous_audio: function (req, res) {
-    sails.log.debug("previous_audio");
-    omx.previous_audio();
+  , next_video: function (req, res) {
+    sails.log.debug("next_video");
+    ffplay.next_video();
     return res.ok();
   }
 
-  , increase_speed: function (req, res) {
-    sails.log.debug("increase_speed");
-    omx.increase_speed();
-    return res.ok();
-  }
-
-  , decrease_speed: function (req, res) {
-    sails.log.debug("decrease_speed");
-    omx.decrease_speed();
+  , full_screen: function (req, res) {
+    sails.log.debug("full_screen");
+    ffplay.full_screen();
     return res.ok();
   }
 };
